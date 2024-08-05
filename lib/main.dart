54af9d144bed6ba42707a967c31df7c442108f14
pranjay-t <pranjayt90@gmail.com>
2024-08-05 11:07:40 +0530

    by pranjay

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Google',
+      title: 'Google_clone',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: backgroundColor,
       ),
